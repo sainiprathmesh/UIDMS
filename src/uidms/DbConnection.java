7edfb753fb581054db1771c2a247f064fe93c5b3
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-02-10 20:03:44 +0530

    class completed : DbConnection

--- src/uidms/DbConnection.java
+++ src/uidms/DbConnection.java
@@ -1,4 +1,20 @@
 package uidms;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
+    public static Connection conn = null;
+
+    void connection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Connecting to database....");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/UIDM", "root", "");
+            System.out.println("Connected to database successfully....");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
