99ffc09cf4c562043e138686036bb5fe99d5f0ef
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-02-10 20:13:58 +0530

    sql file added : UIDM

---
+++ src/UIDM.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 10, 2020 at 03:42 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `UIDM`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `name` varchar(35) NOT NULL,
+  `fname` varchar(35) NOT NULL,
+  `mname` varchar(35) NOT NULL,
+  `dob` date NOT NULL,
+  `mobno` int(12) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `addline` varchar(35) NOT NULL,
+  `district` varchar(30) NOT NULL,
+  `area` varchar(30) NOT NULL,
+  `pincode` int(6) NOT NULL,
+  `uid` int(12) NOT NULL,
+  `pass` varchar(20) NOT NULL,
+  `occupation` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
