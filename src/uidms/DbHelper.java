52db84a0a52dbb774bf8bb18b315e9333e5ced65
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-02-11 09:51:37 +0530

    method added : insertDetail

--- src/uidms/DbHelper.java
+++ src/uidms/DbHelper.java
@@ -1,4 +1,7 @@
 package uidms;
 
 public class DbHelper {
+    void insertDetail(String name, String fName, String mName, String dob, int mobNo, String email, String addressLine, String district, String area, int pin, String uid, String password, String occupation) {
+
+    }
 }
